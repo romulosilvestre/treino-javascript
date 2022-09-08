9494b7b3f76a5e4eab752055301897d5561f8227
romulosilvestre <romulo.silvestre@df.estudante.senai.br>
2022-09-08 10:23:15 -0300

    treino

--- js/ex1_5-1.js
+++ js/ex1_5-1.js
@@ -4,10 +4,7 @@ function calcularDistancia(){
        disTotal:"1492.56",
        disPercorrida:"1000",
    } 
-
    document.getElementById("disTotal").innerHTML = gps.disTotal
    document.getElementById("disPercorrida").innerHTML = gps.disPercorrida
    document.getElementById("disRestante").innerHTML = gps.disTotal-gps.disPercorrida
-
-
 }
